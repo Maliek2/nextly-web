f62e5cc5aec910e67e9481a10e9e32b4f44fb84a
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-10-07 20:10:32 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -38,6 +38,18 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap"
             rel="stylesheet"
           />
+          <script 
+            src="https://imasdk.googleapis.com/js/sdkloader/gpt_proxy.js">
+         </script>
+           <script async
+       data-ad-frequency-hint="30s"
+       src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-123456789" # your publisher ID
+       crossorigin="anonymous">
+   </script>
+   <script>
+      window.adsbygoogle = window.adsbygoogle || [];
+      var adBreak = adConfig = function(o) {adsbygoogle.push(o);}
+   </script>
         </Head>
         <body>
           <Main />
