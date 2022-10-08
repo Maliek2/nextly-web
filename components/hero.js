2816f61182d5d102cc9b2c02fb529007a99782e7
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-10-08 19:58:58 +0200

    Update hero.js

--- components/hero.js
+++ components/hero.js
@@ -9,7 +9,7 @@ export default function Hero() {
         <div className="flex items-center w-full lg:w-1/2">
           <div className="max-w-2xl mb-8">
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
-              Best News Page Template for startups
+              Best News Page Template
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
               Nextly is a free landing page & marketing website
@@ -19,7 +19,7 @@ export default function Hero() {
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
               <a
-                href="https://www.adball.online/amp/templates/nextly"
+                href="https://www.adball.online/amp/"
                 target="_blank"
                 rel="noopener"
                 className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md ">
