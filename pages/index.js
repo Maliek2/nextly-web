cc50750cebf2321f13737d32c07769ac9116b830
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-10-09 19:32:02 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Hero from "../components/hero";
 import Navbar from "../components/navbar";
 import SectionTitle from "../components/sectionTitle";
-
+import Amp from "/amp";
 import { benefitOne, benefitTwo } from "../components/data";
 import Video from "../components/video";
 import Benefits from "../components/benefits";
