465b33421ff723a895f8d928dba912b9973dd789
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-10-09 19:37:23 +0200

    Update and rename Amp.js to amp.js

--- components/amp.js
+++ components/amp.js
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import events from '../components/events.js';
 import * as Runtimes from '../components/runtimes.js';
 
 const PARAM_MODE = 'mode';
